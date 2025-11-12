9def253b8301bc10eae1f441af3d67cb0d733e58
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-11-12 17:43:07 +0530

    full stack--

---
+++ FULL-STACK/SERVER/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
